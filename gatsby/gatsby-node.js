8511981b274a0620d364635628dd4b17b8870205
Laura Tejada <laura_mtc@hotmail.com>
2020-11-02 15:53:05 -0500

    adding pizza, topping and beers pages

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,96 @@
+import path from 'path';
+import fetch from 'isomorphic-fetch';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  // Loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // the url for this new page
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+async function turnToppingsIntoPages({ graphql, actions }) {
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          id
+          name
+        }
+      }
+    }
+  `);
+
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        slug: topping.name,
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+}
+
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  const beers = await res.json();
+
+  // Create a node for each beer
+  for (const beer of beers) {
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+export async function sourceNodes(params) {
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
+}
+
+export async function createPages(params) {
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
+}
