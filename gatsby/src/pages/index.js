a9731ec7a5752eda843a8222661cc890b87d6334
Laura Tejada <laura_mtc@hotmail.com>
2020-11-18 21:53:18 -0500

    fetching store settings data from the client

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>Currently Slicing</p>
+    </div>
+  );
+}
+
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { hotSlices, slicemasters } = useLatestData();
+
   return (
-    <>
-      <p>Hey I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The best pizza downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
   );
 }
